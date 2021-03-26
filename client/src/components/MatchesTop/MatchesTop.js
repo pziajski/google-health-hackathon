9fbdae548c52047d129e4bc367f564eba171e7a0
Patrick <patrick.ziajski@gmail.com>
2021-03-26 05:29:32 -0400

    coverage top module

--- client/src/components/MatchesTop/MatchesTop.js
+++ client/src/components/MatchesTop/MatchesTop.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import MatchesTopItem from "../MatchesTopItem/MatchesTopItem";
+import "./MatchesTop.scss";
 
-export default function MatchesTop() {
+export default function MatchesTop(props) {
+    const temp = [
+        {
+            id: "1",
+            name: "Manulife",
+            image: "",
+            match: "85%"
+        },
+        {
+            id: "2",
+            name: "Kaiser Health",
+            image: "",
+            match: "83%"
+        },
+        {
+            id: "3",
+            name: "Humana",
+            image: "",
+            match: "75%"
+        },
+        {
+            id: "4",
+            name: "CVS",
+            image: "",
+            match: "72%"
+        },
+        {
+            id: "5",
+            name: "WellCare",
+            image: "",
+            match: "70%"
+        },
+    ];
+
     return (
-        <div>
-            <h2>Matches Top</h2>
+        <div className="matches-top">
+            {
+                temp.map(company => {
+                    return <MatchesTopItem key={company.id} name={company.name} image={company.image} match={company.match} />
+                })
+            }
         </div>
     )
 }
