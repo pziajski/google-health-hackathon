f464d8e254b43d9ee2c8a28a179ab835dbcfdc01
Patrick <patrick.ziajski@gmail.com>
2021-03-26 05:28:42 -0400

    seperated matching components to respective pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import { Component } from 'react';
 import axios from 'axios';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import NavBar from './component/nav/NavBar'
-import SurveyPage2 from "./pages/SurveyPage2/SurveyPage2";
-import SurveyPage3 from "./pages/SurveyPage3/SurveyPage3";
 import OnbordingPage1 from './pages/Onbording/OnboardingPage1';
-import './App.scss';
+import TopNavBar from "./components/TopNavBar/TopNavBar";
 import MatchesPage from './pages/MatchesPage/MatchesPage';
+import SurveyPage from "./pages/SurveyPage/SurveyPage";
+import './App.scss';
 
 class App extends Component {
-    state = {
-        insurance: null,
-        hospitals: null,
-        user: null
+    constructor() {
+        super();
+        this.state = {
+            insurance: null,
+            hospitals: null,
+            user: null
+        }
     }
 
     componentDidMount() {
@@ -26,14 +28,18 @@ class App extends Component {
         //     });
     }
 
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
     render() {
         return (
             <div className="App">
+                <TopNavBar title="Google Health"/>
                 <BrowserRouter>
                 <Switch>
-                    <Route path='/survey/1' render={(renderProps) => <OnbordingPage1 {...renderProps} />}/>
-                    <Route path="/survey/2" render={(renderProps) => <SurveyPage2 {...renderProps} />} />
-                    <Route path="/survey/3" render={(renderProps) => <SurveyPage3 {...renderProps} />} />
+                    <Route path='/' exact render={(renderProps) => <OnbordingPage1 {...renderProps} />}/>
+                    <Route path="/survey" render={renderProps => <SurveyPage {...renderProps} />}/>
                     <Route path="/matches" render={(renderProps) => <MatchesPage {...renderProps} />} />
                 </Switch>
                 </BrowserRouter>
