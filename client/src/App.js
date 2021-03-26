c6101d918d4566c0690995a0e131fda828e8f075
Patrick <patrick.ziajski@gmail.com>
2021-03-25 21:51:24 -0400

    spacing

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
 import { Component } from 'react';
 import axios from 'axios';
 
-
-
 class App extends Component {
-
-  state = {
-    insurance:null,
-    hospitals:null,
-    user:null
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:8080/insurance')
-      .them(res => {
-        this.setState({insurance:res.data});
-      });
-      axios.get('http://localhost:8080/hospitals')
-      .them(res => {
-        this.setState({hospitalsance:res.data});
-      });
+    state = {
+        insurance: null,
+        hospitals: null,
+        user: null
     }
 
-  render() {
-    return (
-      <div className="App">
+    componentDidMount() {
+        axios.get('http://localhost:8080/insurance')
+            .them(res => {
+                this.setState({ insurance: res.data });
+            });
+        axios.get('http://localhost:8080/hospitals')
+            .them(res => {
+                this.setState({ hospitalsance: res.data });
+            });
+    }
 
-      </div>
-    );
+    render() {
+        return (
+            <div className="App">
 
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
