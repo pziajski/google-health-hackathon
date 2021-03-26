94e1d9ab931cd3c7bf11997488d4a7c421d9a180
Patrick <patrick.ziajski@gmail.com>
2021-03-25 21:14:27 -0400

    made sure to allow json responses

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
 app.use("/insurance", insuranceRouter);
 app.use("/hospitals", hospitalsRouter);
 
