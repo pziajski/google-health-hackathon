129210a6000cb6a658934bdaab6e776dfe6852ad
Patrick <patrick.ziajski@gmail.com>
2021-03-26 04:05:45 -0400

    added matches to app route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import SurveyPage2 from "./pages/SurveyPage2/SurveyPage2";
 import SurveyPage3 from "./pages/SurveyPage3/SurveyPage3";
 import OnbordingPage1 from './pages/Onbording/OnboardingPage1';
 import './App.scss';
+import MatchesPage from './pages/MatchesPage/MatchesPage';
 
 class App extends Component {
     state = {
@@ -32,6 +33,7 @@ class App extends Component {
                     <Route path='/survey/1' render={(renderProps) => <OnbordingPage1 {...renderProps} />}/>
                     <Route path="/survey/2" render={(renderProps) => <SurveyPage2 {...renderProps} />} />
                     <Route path="/survey/3" render={(renderProps) => <SurveyPage3 {...renderProps} />} />
+                    <Route path="/matches" render={(renderProps) => <MatchesPage {...renderProps} />} />
                 </Switch>
                 </BrowserRouter>
             </div>
