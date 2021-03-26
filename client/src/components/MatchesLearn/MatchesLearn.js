34fa3336c98e0a698886b60140200eac92cd79e9
Patrick <patrick.ziajski@gmail.com>
2021-03-26 13:51:34 -0400

    working menu idicators

--- client/src/components/MatchesLearn/MatchesLearn.js
+++ client/src/components/MatchesLearn/MatchesLearn.js
@@ -3,12 +3,13 @@ import TopNavBar from "../../components/TopNavBar/TopNavBar";
 
 export default class MatchesLearn extends Component {
     constructor(props) {
+        console.log("yes")
         super();
         this.props = props;
     }
 
     render() {
-        console.log(this.props.match.params)
+        console.log(this.props)
         return (
             <div className="matches-learn">
                 <TopNavBar title={this.props.match.params.id} menuIcon="back" />
