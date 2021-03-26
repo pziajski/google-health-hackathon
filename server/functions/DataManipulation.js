e5615bb19085faa346a82b2bceb6511b01ea510a
Patrick <patrick.ziajski@gmail.com>
2021-03-25 21:14:09 -0400

    exported functions

--- server/functions/DataManipulation.js
+++ server/functions/DataManipulation.js
@@ -7,3 +7,5 @@ const ReadFromFile = (filePath) => {
 const WriteToFile = (filepath, obj) => {
     writeFileSync(filepath, JSON.stringify(obj), (e) => { console.log(e) })
 }
+
+module.exports = { ReadFromFile, WriteToFile };
